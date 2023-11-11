806231a01bb05455aa0a975a9ebe26cf61bcda53
obel <rdebebe@kifiya.et>
2023-11-11 23:39:49 +0300

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactIcons from 'react-icons';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [reactIcons],
+      external: ['react-icons/fa', 'react-icons/ai', 'react-icons/bs'],
     },
+  
   },
 })
