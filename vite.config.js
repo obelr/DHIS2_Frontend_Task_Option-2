fe2502b907524831ba01337df7b210371c67a47e
obel <rdebebe@kifiya.et>
2023-11-11 23:35:52 +0300

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactIconsFa from 'react-icons/fa';
+import { AiOutlineBarChart } from 'react-icons/ai';
+import { BsMap, BsFillChatRightTextFill } from 'react-icons/bs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [AiOutlineBarChart, BsMap, BsFillChatRightTextFill,reactIconsFa],
+    },
+  },
 })
