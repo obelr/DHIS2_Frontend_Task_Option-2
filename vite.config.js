56f5bb4ae9590c024ff9be7488dc4c1d47a55a18
obel <rdebebe@kifiya.et>
2023-11-11 23:38:07 +0300

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactIconsFa from 'react-icons/fa';
-import { AiOutlineBarChart } from 'react-icons/ai';
-import { BsMap, BsFillChatRightTextFill } from 'react-icons/bs';
+import reactIcons from 'react-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [AiOutlineBarChart, BsMap, BsFillChatRightTextFill,reactIconsFa],
+      external: [reactIcons],
     },
   },
 })
