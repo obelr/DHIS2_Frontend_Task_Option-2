4a7f07ff8f35613449a63430795e0395d98d7ff0
obel <rdebebe@kifiya.et>
2023-11-12 18:04:01 +0300

    update path to absolute

--- src/components/DashboardDetails.jsx
+++ src/components/DashboardDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo, memo } from "react";
 import { useDashboardContext } from "../context/DashboardContext";
-import { AiOutlineBarChart } from 'react-icons/ai';
-import { BsMap, BsFillChatRightTextFill } from 'react-icons/bs';
+import { AiOutlineBarChart } from '/node_modules/react-icons/ai';
+import { BsMap, BsFillChatRightTextFill } from '/node_modules/react-icons/bs';
 
 const DashboardDetails = memo(({ dashboard }) => {
   const { state } = useDashboardContext();
